fef03f75973dfe921ce303457b0ac06a56c4c986
Andrew Grimm <andrew.grimm@terem.com.au>
2015-09-03 16:41:11 +1000

    NOTICKET Background jobs: Run generator and migration for delayed job

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150720070713) do
+ActiveRecord::Schema.define(:version => 20150903063350) do
 
   create_table "access_conditions", :force => true do |t|
     t.string   "name"
@@ -126,6 +126,22 @@ ActiveRecord::Schema.define(:version => 20150720070713) do
   end
 
   add_index "data_categories", ["name"], :name => "index_data_categories_on_name", :unique => true
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0, :null => false
+    t.integer  "attempts",   :default => 0, :null => false
+    t.text     "handler",                   :null => false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "discourse_types", :force => true do |t|
     t.string "name", :null => false
