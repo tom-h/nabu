6fdbc9476b6425d9d2560dfccd561797980a2586
Andrew Grimm <andrew.grimm@terem.com.au>
2015-09-11 17:10:15 +1000

    Nabu-452 WIP: Add specs showing what does and does not work

--- spec/features/search_items_spec.rb
+++ spec/features/search_items_spec.rb
@@ -1,6 +1,67 @@
 require 'spec_helper'
 describe 'Item Search', search: true do
   describe 'Solr searching of items' do
+    let(:search) do
+      Item.solr_search do
+        fulltext search_term
+      end
+    end
+    before do
+      Sunspot.remove_all!(Item)
+      Sunspot.index!(item)
+    end
+    after do
+      Sunspot.remove_all!(Item)
+    end
+
+    context 'searching by a keyword mentioned in language' do
+      let(:language) { 'South Efate, Bislama' }
+      let!(:item) { create(:item, language: language) }
+      let(:search_term) { language }
+      it 'should have a match' do
+        expect(search.results.length).to eq 1
+      end
+    end
+
+    context 'searching by identifier' do
+      let(:identifier) { 'SomeWords' }
+      let!(:item) { create(:item, identifier: identifier) }
+
+      context 'using a full keyword' do
+        let(:search_term) { identifier }
+        it 'should have a match' do
+          expect(search.results.length).to eq 1
+        end
+      end
+
+      context 'using a partial keyword' do
+        let(:search_term) { identifier[0..-2] }
+        it 'should have a match' do
+          expect(search.results.length).to eq 1
+        end
+      end
+    end
+
+    context 'searching by full identifier' do
+      let(:identifier) { 'OtherWords' }
+      let!(:item) { create(:item, identifier: identifier) }
+
+      context 'using a full keyword' do
+        let(:search_term) { item.full_identifier }
+        it 'should have a match' do
+          pending 'fails for some reason'
+          expect(search.results.length).to eq 1
+        end
+      end
+
+      context 'using a partial keyword' do
+        let(:search_term) { item.full_identifier[0..-2] }
+        it 'should have a match' do
+          pending 'fails for some reason'
+          expect(search.results.length).to eq 1
+        end
+      end
+    end
   end
 
   describe 'ItemsController use of Item Search' do
