42071f72c8a30c94e0272c9d6654456f365f6e34
Andrew Grimm <andrew.grimm@terem.com.au>
2016-03-08 10:55:47 +1100

    Issue 490 WIP: Disable bullet alerts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,7 @@ Nabu::Application.configure do
 
   config.after_initialize do
     Bullet.enable = true
-    Bullet.alert = true
+    # Bullet.alert = true
     Bullet.bullet_logger = true
     Bullet.console = true
     # Bullet.growl = true
