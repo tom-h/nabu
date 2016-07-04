55802917955988072afd741ff9cc4e55cc3a2dd3
Andrew Grimm <andrew.grimm@terem.com.au>
2016-07-04 18:15:32 +1000

    Issue #591 Fix Rendering WIP: Refactor rake task

---
+++ app/services/export_metadata_service.rb
@@ -0,0 +1,38 @@
+class ExportMetadataService
+  def self.run(file, basename, coll_id, item_id, item, verbose)
+    export_metadata_service = new(file, basename, coll_id, item_id, item, verbose)
+    export_metadata_service.run
+  end
+
+  def initialize(file, basename, coll_id, item_id, item, verbose)
+    @file = file
+    @basename = basename
+    @coll_id = coll_id
+    @item_id = item_id
+    @item = item
+    @verbose = verbose
+  end
+
+  def run
+    # if metadata files exist, skip to the next file
+    metadata_filename_imp = Nabu::Application.config.write_imp + @basename + ".imp.xml"
+    metadata_filename_id3 = Nabu::Application.config.write_id3 + @basename + ".id3.v2_3.xml"
+    return if (File.file? "#{metadata_filename_imp}") && (File.file? "#{metadata_filename_id3}")
+
+    # check if the item's "metadata ready for export" flag is set
+    # raise a warning if not and skip file
+    if !@item.metadata_exportable
+      puts "ERROR: metadata of item pid=#{@coll_id}-#{@item_id} is not complete for file #{@file} - skipping" if @verbose
+      return
+    end
+
+    template = ItemOfflineTemplate.new
+    template.item = @item
+    data_imp = template.render_to_string :template => "items/show.imp.xml"
+    data_id3 = template.render_to_string :template => "items/show.id3.xml"
+
+    File.open(metadata_filename_imp, 'w') {|f| f.write(data_imp)}
+    File.open(metadata_filename_id3, 'w') {|f| f.write(data_id3)}
+    puts "SUCCESS: metadata files\n #{metadata_filename_imp},\n #{metadata_filename_id3}\n created for #{@file}"
+  end
+end
--- lib/tasks/archive.rake
+++ lib/tasks/archive.rake
@@ -52,26 +52,7 @@ namespace :archive do
       basename, _, coll_id, item_id, collection, item = parse_file_name(file, 'wav')
       next if !collection || !item
 
-      # if metadata files exist, skip to the next file
-      metadata_filename_imp = Nabu::Application.config.write_imp + basename + ".imp.xml"
-      metadata_filename_id3 = Nabu::Application.config.write_id3 + basename + ".id3.v2_3.xml"
-      next if (File.file? "#{metadata_filename_imp}") && (File.file? "#{metadata_filename_id3}")
-
-      # check if the item's "metadata ready for export" flag is set
-      # raise a warning if not and skip file
-      if !item.metadata_exportable
-        puts "ERROR: metadata of item pid=#{coll_id}-#{item_id} is not complete for file #{file} - skipping" if verbose
-        next
-      end
-
-      template = ItemOfflineTemplate.new
-      template.item = item
-      data_imp = template.render_to_string :template => "items/show.imp.xml"
-      data_id3 = template.render_to_string :template => "items/show.id3.xml"
-
-      File.open(metadata_filename_imp, 'w') {|f| f.write(data_imp)}
-      File.open(metadata_filename_id3, 'w') {|f| f.write(data_id3)}
-      puts "SUCCESS: metadata files\n #{metadata_filename_imp},\n #{metadata_filename_id3}\n created for #{file}"
+      ExportMetadataService.run(file, basename, coll_id, item_id, item, verbose)
     end
   end
 
